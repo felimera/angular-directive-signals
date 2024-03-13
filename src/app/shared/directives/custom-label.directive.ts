87d91cfedce39470632df1d98f412a9dce8cb854
andres.mera <feli041089@gmail.com>
2024-03-13 10:49:45 -0500

    developer : Custom Label - Directiva personalizada.

---
+++ src/app/shared/directives/custom-label.directive.ts
@@ -0,0 +1,16 @@
+import { Directive, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[customLabel]'
+})
+export class CustomLabelDirective {
+
+  private htmlElement?: ElementRef<HTMLElement>;
+  constructor(
+    private el: ElementRef<HTMLElement>
+  ) {
+    console.log(el);
+    this.htmlElement = el;
+  }
+
+}
