1e7923cd11c0ca14d4a98473a0c4c2416fa04cf6
andres.mera <feli041089@gmail.com>
2024-03-13 10:28:30 -0500

    developer : Formulario reactivo tradicional.

--- src/app/products/pages/product-page/product-page.component.ts
+++ src/app/products/pages/product-page/product-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './product-page.component.html',
@@ -6,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class ProductPageComponent {
 
+  // constructor(private fb:FormBuilder){}
+  private fb = inject(FormBuilder);
+
+  public color: string = 'green';
+
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(6), Validators.email]],
+  });
+
+  changeColor(): void {
+    const newColor = '#xxxxxx'.replace(/x/g, y => (Math.random() * 16 | 0).toString(16));
+    this.color = newColor;
+  }
 }
